42e713952dad0ef0f8af681108a28eb848109515
AbdulNa <anapaate03an@gmai.com>
2022-09-16 17:00:04 -0700

    tasks

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,6 +13,7 @@ void print_triangle(int size)
 	else
 {
 	int i, j;
+	
 	for (i = 1; i <= size; i++)
 {
 	for (j = i; j < size; j++)
@@ -20,7 +21,8 @@ void print_triangle(int size)
 	_putchar(' ');
 }
 	for (j = 1; j <= i; j++)
-{	_putchar('#');
+{	
+	_putchar('#');
 }
 	_putchar('\n');
 }
