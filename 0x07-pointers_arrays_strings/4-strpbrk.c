790890bcf63c9443ab7f93c2cd6fdd875eb8d5fd
AbdulNa <anapaate03an@gmai.com>
2022-09-27 19:41:33 -0700

    07 pointers

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,11 +3,11 @@
 
 /**
  **_strpbrk - searches a string for any of a set of bytes
- ** @s: string to search
+ * @s: string to search
  ** @accept: stringcontaining the bytes to look for
  **
- ** Return: pointer to the byte in s that matches one of the bytes in accept
- ** or NULL if no such byte is found
+ * Return: pointer to the byte in s that matches one of the bytes in accept
+ * or NULL if no such byte is found
  **/
 	char *_strpbrk(char *s, char *accept)
 {
@@ -22,7 +22,7 @@
 	return (s);
 }
 }
-	++;
+	s++;
 }
 	return (NULL);
 }
