da9f3a5be23d61b2ec8b067be7a05f55089bf05c
AbdulNa <anapaate03an@gmai.com>
2022-10-06 06:53:12 -0700

    advanced malloc

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- ** argstostr - concatenates all arguments of the program.
- ** @ac: argument count.
- ** @av: pointer to array of size ac.
- ** Return: NULL if ac == 0 or av == null, Pointer to new string.
- ** NULL on fail.
+ **argstostr - concatenates all arguments of the program.
+ **@ac: argument count.
+ **@av: pointer to array of size ac.
+ **Return: NULL if ac == 0 or av == null, Pointer to new string.
+ **NULL on fail.
  **/
 	char *argstostr(int ac, char **av)
 {
@@ -14,18 +14,14 @@
 
 	size = 0;
 	k = 0;
-
 	if (ac == 0 || av == NULL)
 	return (NULL);
-
 	i = 0;
-
 	while (i < ac)
 {
 	j = 0;
-
 	while (av[i][j])
-
+{
 	size++;
 	j++;
 }
@@ -33,12 +29,9 @@
 	i++;
 }
 	arg = malloc((sizeof(char) * size) + 1);
-
 	if (arg == NULL)
 	return (NULL);
-
 	i = 0;
-
 	while (i < ac)
 {
 	j = 0;
@@ -53,6 +46,5 @@
 	i++;
 }
 	arg[k] = '\0';
-
 	return (arg);
 }
